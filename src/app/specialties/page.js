eb74872db67cbe50428ecd35ebe25985fe536968
husneara <husnearaa99@gmail.com>
2024-11-23 18:22:59 +0600

    has completed the specialties page

---
+++ src/app/specialties/page.js
@@ -0,0 +1,12 @@
+import SpecialtiesPage from "@/components/SpecialtesPage/SpecialtiesPage";
+
+
+const page = () => {
+    return (
+        <div>
+          <SpecialtiesPage />  
+        </div>
+    );
+};
+
+export default page;
